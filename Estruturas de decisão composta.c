468fe32f925b851a5ffffe19977b7198abe39546
Wesley3596 <wesleysilva.03@hotmail.com>
2025-04-08 19:11:16 +0000

    Aula 08/04

---
+++ Estruturas de decisão composta.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+
+int main(){
+    int numero1;
+    int numero2;
+
+    numero1 = 2;
+    numero2 = 2 ;
+
+    if (numero1 >= numero2) {
+        printf("O numero 1 é maior ou igual\n");
+       }else {
+        printf("O numero 2 é maior");
+       }
+
+       return 0;
+
+
+}
